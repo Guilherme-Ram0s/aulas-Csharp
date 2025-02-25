25b224235ec93e8e574c96b737e67c262b61030b
Guilherme-Ram0s <gr4244888@gmail.com>
2025-02-25 20:26:20 -0300

    calculadora

--- aula1/aula1/Program.cs
+++ aula1/aula1/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("aaaaaaaaaaaaa");
+int num1, num2;
+string operacao;
+int resultado;
+string aceitar;
+Console.WriteLine("Bem vindo a primeira calculadora em c#\n");
+
+
+Console.WriteLine("digite qual operação");
+operacao = Console.ReadLine();
+
+    if (operacao == "+")
+    {
+        Console.WriteLine("Digite o primeiro número");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 + num2;
+        Console.WriteLine($"O resultado da soma é: {resultado}");
+    }
+    else if (operacao == "-")
+    {
+        Console.WriteLine("Digite o primeiro número");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 - num2;
+        Console.WriteLine($"O resultado da subtração é: {resultado}");
+    }
+    else if (operacao == "*")
+    {
+        Console.WriteLine("Digite o primeiro número");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 * num2;
+        Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+    }
+    else if (operacao == "/")
+    {
+        Console.WriteLine("Digite o primeiro número");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 / num2;
+        Console.WriteLine($"O resultado da divisão é: {resultado}");
+    }
+else
+    {
+        Console.WriteLine("Operação inválida");
+    }
+
+
+
