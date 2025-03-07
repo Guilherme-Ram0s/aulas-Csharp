591f457ea04fd5fe92afeef975a92364165a41bd
Guilherme-Ram0s <gr4244888@gmail.com>
2025-03-06 21:09:06 -0300

    aula de classes, metodos e atributos

---
+++ aula3/ConsoleApp1/principal.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class principal
+    {
+        static void Main(string[] args)
+        {
+
+            Produto teclado = new Produto("teclado", 59, 92.10);
+            Produto comida = new Produto("pizza", 10, 40);
+
+            Console.WriteLine(teclado.getDetailProduct());
+            Console.WriteLine(comida.getDetailProduct());
+        }
+    }
+}
---
+++ aula3/ConsoleApp1/produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Produto
+    {
+        public string Nome;
+        public int QtdEstoque;
+        public double Preco;
+
+        public Produto(string nome, int qtdEstoque, double preco)
+        {
+            Nome = nome;
+            QtdEstoque = qtdEstoque;
+            Preco = preco;
+        }
+
+        public double getTotalAmount()
+        { 
+            return Preco * QtdEstoque;
+        }
+        public string getDetailProduct()
+        {
+            return $"Nome: {Nome} \npreço: {Preco} \nquantidade: {QtdEstoque}";
+        }
+
+    }
+}
